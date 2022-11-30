eccc5f7d455447cb47046a3e457051d334f26268
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-11-29 19:14:16 -0500

    add code

---
+++ LoadData.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace puyoQuestDL
+{
+    public class MasterDataMap
+    {
+        public MasterArchiveData[] master_archive_data_array { get; set; }
+    }
+
+    [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
+    public class MasterArchiveData
+    {
+        public sbyte download_type { get; set; }
+        public string archive_url { get; set; }
+        public long check_sum { get; set; }
+        public long archive_size { get; set; }
+        public int archive_id { get; set; }
+    }
+
+    public class LoadData
+    {
+        public MasterDataMap master_data_map { get; set; }
+    }
+}
---
+++ PuyoQuestDL.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace puyoQuestDL
+{
+    class PuyoQuestDL
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: puyoQuestDL load_data.json [output directory]");
+                return;
+            }
+
+            string jsonF = args[0];
+            string outDir = args[1];
+
+            string json;
+
+            using (StreamReader r = new(jsonF))
+            {
+                json = r.ReadToEnd();
+            }
+
+            LoadData loadData = JsonSerializer.Deserialize<LoadData>(json);
+
+            StringBuilder sb = new();
+
+            string url;
+
+            WebClient webClient = new();
+            for (int i = 0; i < loadData.master_data_map.master_archive_data_array.Length; i++)
+            {
+                url = loadData.master_data_map.master_archive_data_array[i].archive_url;
+
+                string fileName = url.Replace("https://pyq-cf-dl.sega-net.jp/", string.Empty);
+
+                sb.Clear();
+
+                sb.Append(outDir);
+                sb.Append('\\');
+                sb.Append(fileName);
+
+                Console.WriteLine("Downloading " + url);
+                webClient.DownloadFile(url, sb.ToString());
+            }
+
+            Console.WriteLine("\nDone!");
+        }
+    }
+}
